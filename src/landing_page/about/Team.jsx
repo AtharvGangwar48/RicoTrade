efbd7a510733784aa950e3f59fa5d284ce278876
Atharv Gangwar <152419242+AtharvGangwar48@users.noreply.github.com>
2025-01-19 23:55:31 +0530

    Update Team.jsx

--- src/landing_page/about/Team.jsx
+++ src/landing_page/about/Team.jsx
@@ -19,6 +19,20 @@ function Team() {
                 <p>Playing basketball is his zen.</p>
             </div>
         </div>
+        <div className="row">
+            <div className="col-6">
+                <br />
+                <br />
+                <img src="media/Atharv.jpg" alt="Atharv" style={{borderRadius:'30%', height:'17rem'}} />
+                <h5><br />Atharv Gangwar <br /></h5>
+                <p className='text-muted fs-15'>Web Developer, Created RicoTrade </p>
+            </div>
+            <div className="col-6" style={{textAlign:'start', padding:'0px 50px 20px 0px'}}>
+                <br />
+                <p><br /><br /> Hello, I am Atharv Gangwar, a passionate web developer who has been actively working in this field since November 2023. I have a strong enthusiasm for creating functional and visually appealing web applications. This is my first project using React, and I am excited to present RicoTrade, a trading platform designed as a clone of Zerodha. Through this project, I aimed to replicate and enhance key features of a professional trading platform while honing my skills in React and web development.</p>
+                <p> If you'd like to explore more of my work, feel free to visit my GitHub profile: <a href="https://github.com/AtharvGangwar48">Atharv48</a>, where I showcase other exciting projects that reflect my dedication and growth in this domain. </p>
+            </div>
+        </div>
        </div>
      );
 }
